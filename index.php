6f22f466e6ed0b6f88a05603006b7cce1069ad31
Kristjan Põldmets <krispol@users.noreply.git.com>
2018-09-11 14:22:20 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Siin lehel leidub igast asju">
+	<meta name="author" content="Kristjan Põldmets">
+	<meta name="veiwport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="cascadingstylesheets.css">
+</head>
+<body>
+	<title>Yarr, there be kittens!</title>
+	<h1>Yarr, there be kittens!</h1>
+	<img src="kitten.jfif" alt="Yarrkitty" title="a cute pussycat">
+	<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ" title="TLÜ">
+	<p>See leht on valmistatud <a href="https://www.tlu.ee/">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu</p>
+	<p>See tekst siin on lisatud koduarvutis</p>
+	<p>Minu sõps teeb ka <a href="../../~laurrau/" target="blank">veebi</a></p>
+</body>
+</html>
